370ae55b16720456757787bab420c878bed86893
elliottrarmstrong <elliott.armstrong.17@cnu.edu>
2019-09-10 19:54:18 -0400

    got rid of myexe and compiled successfully

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 //#includes are ALWAYS at top of file
 #include <iostream>	//iostream is where std::cout and std::endl are defined
+#include <string.h>
 #include "myfunc.h"
 
 int main()
 {
-	std::cout<<func()<<std::endl;
+	std::string a = func();
+	std::cout<<a<<std::endl;
 	return 0;
 }
